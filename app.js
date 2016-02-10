18b3cc9082f010af66ef5fcaa5e6197875ef3b6a
MichaelMacLeod <edinburghmacleod@gmail.com>
2016-02-10 08:16:10 +0000

    Added Express Layouts and EJS

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 var express = require('express');
 var app = express();
 var http = require('http');
+var expressLayouts = require('express-ejs-layouts');
+
 
 app.get('/weather', function(req, res) {
   res.send('Hello World!') 
 });
+
+
+//App settings
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+app.use(expressLayouts);
+app.use(express.static('public'));
+
+app.get('/weather', function(req, res) {
+    res.render('weather')
+});
+
 
 //CALLBACK: Getting the data from the API server to display it to us: 
 app.get('/weather/:location', function(request, response) {
